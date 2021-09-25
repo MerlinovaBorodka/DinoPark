e9fdcd6f6e7d1f90b658605dfacad7e078697d0e
MerlinovaBoroda <>
2021-09-25 21:16:06 +0000

    Initial commit

---
+++ player.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "anim_sprite.h"
+
+using namespace std;
+
+class Player: public DirectedSprite{
+  //protected:
+  public:
+  int hp, speed,hungry;
+
+  public:
+
+  Player() : hp(100), speed(1), hungry(0) {}
+
+  Player(string spriteSrc, 
+        int frameCount, 
+        int x = 0, int y = 0, 
+        string direction = "right",
+        int hp=100, int sp=1, int hun=0)
+          :DirectedSprite(spriteSrc,  frameCount,  x , y , direction),
+          hp(hp), speed(sp), hungry(hun)
+    {}
+
+  
+
+    void goLeft() 
+    {if(x > speed) {x-=speed;
+    hungry++;}
+    if(hungry == 10) hp--; }
+    void goRight() 
+    {x+=speed;hungry++;
+    if(hungry == 10) hp--; }
+    void goUp() 
+    {if(y > speed) {y-=speed;
+    hungry++;}
+    if(hungry == 10) hp--; }
+    void goDown() 
+    {y+=speed;hungry++;
+    if(hungry == 10) hp--; }
+
+};
